2475b86d17de42c229cbfed394ef2e09b3cc33f6
Grissia <wasd.lhe@gmail.com>
2024-12-08 10:07:23 +0800

    feat: support exchange service on docker

--- exchange/app.py
+++ exchange/app.py
@@ -54,4 +54,4 @@ def assign_gifts(participants):
     return {sender: receiver for sender, receiver in zip(participants, receivers)}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
